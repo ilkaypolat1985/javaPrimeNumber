7e1cf5b63c42f69e0fe73b1bf9668b0d59b78e7d
ilkaypolat1985 <ilkaypolat1985@gmail.com>
2019-12-27 17:30:51 +0100

    Formatted

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-	    int number = 4;
+        int number = 4;
 
-	 	if(isPrimeNumber(number)){
-			System.out.println(number + " is a prime number!");
-		}else{
-			System.out.println(number + " is not a prime number!");
-		}
+        if (isPrimeNumber(number)) {
+            System.out.println(number + " is a prime number!");
+        } else {
+            System.out.println(number + " is not a prime number!");
+        }
     }
 
-    public static boolean isPrimeNumber(int number){
+    public static boolean isPrimeNumber(int number) {
 
-    	if(number < 2) return false;
+        if (number < 2) return false;
 
-    	for(int i = 2; i < number/2; i++){
-			if(number % i == 0){
-				System.out.println(number + " is not a prime number!");
-				return false;
-			}
-		}
+        for (int i = 2; i < number / 2; i++) {
+            if (number % i == 0) {
+                System.out.println(number + " is not a prime number!");
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
 }
